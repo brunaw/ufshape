9c292f55dd6a86e002c94e638ffb88351a8ff8a9
jtrecenti <julio.trecenti@gmail.com>
2018-05-29 21:16:12 -0300

    upload package

---
+++ R/df_uf.R
@@ -0,0 +1,13 @@
+#' Brazil state polygons
+#'
+#' An \code{sf} object to draw maps
+#'
+#' @format A data frame with 27 rows and 2 variables:
+#' \describe{
+#'    \item{NM_ESTADO}{State name}
+#'    \item{NM_REGIAO}{Region name}
+#'    \item{CD_GEOCUF}{UF code}
+#'    \item{geometry}{multipolygon}
+#' }
+#' @source IBGE.
+"df_uf"
